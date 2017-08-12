872f420570a718d3baf8b55513b9b275fdb399bb
Bojie Jiang <jiangbojie@gmail.com>
2017-08-12 10:52:12 -0700

    First RESTFUL api

--- src/main/java/us/bojie/imbo/push/service/AccountService.java
+++ src/main/java/us/bojie/imbo/push/service/AccountService.java
@@ -1,7 +1,13 @@
 package us.bojie.imbo.push.service;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import us.bojie.imbo.push.bean.User;
 
 // 127.0.0.1/api/account
 @Path("/account")
@@ -12,5 +18,16 @@ public class AccountService {
     @Path("/login")
     public String get() {
         return "You get the login";
+    }
+
+    @POST
+    @Path("/login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public User post() {
+        User user = new User();
+        user.setName("Bojie");
+        user.setSex(1);
+        return user;
     }
 }
