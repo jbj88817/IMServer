0aad6bb4a27150600c218fc24e6690a3e70a9281
Bojie Jiang <jiangbojie@gmail.com>
2017-10-05 23:17:34 -0700

    Modified config file

--- src/main/java/us/bojie/imbo/push/Application.java
+++ src/main/java/us/bojie/imbo/push/Application.java
@@ -1,17 +1,23 @@
 package us.bojie.imbo.push;
 
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.util.logging.Logger;
 
+import us.bojie.imbo.push.provider.GsonProvider;
 import us.bojie.imbo.push.service.AccountService;
 
 public class Application extends ResourceConfig {
     public Application() {
+        // 注册逻辑处理的包名
         packages(AccountService.class.getPackage().getName());
-        register(JacksonJsonProvider.class);
+
+        // 注册Json解析器
+//        register(JacksonJsonProvider.class);
+        // 替换解析器为Gson
+        register(GsonProvider.class);
+
+        // 注册日志打印输出
         register(Logger.class);
     }
 }
