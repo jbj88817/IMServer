a50cdc16043e80aedb68b844e3c34ffca2f12c16
Bojie Jiang <jiangbojie@gmail.com>
2017-10-09 22:38:11 -0700

    RegisterModel

--- src/main/java/us/bojie/imbo/push/service/AccountService.java
+++ src/main/java/us/bojie/imbo/push/service/AccountService.java
@@ -1,33 +1,26 @@
 package us.bojie.imbo.push.service;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import us.bojie.imbo.push.bean.db.User;
+import us.bojie.imbo.push.bean.api.account.RegisterModel;
 
 // 127.0.0.1/api/account
 @Path("/account")
 public class AccountService {
 
-    // 127.0.0.1/api/account/login
-    @GET
-    @Path("/login")
-    public String get() {
-        return "You get the login";
-    }
-
     @POST
-    @Path("/login")
+    @Path("/register")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public User post() {
-        User user = new User();
-        user.setName("Bojie");
-        user.setSex(1);
-        return user;
+    public RegisterModel register(RegisterModel model) {
+        return model;
+//        User user = new User();
+//        user.setName("Bojie");
+//        user.setSex(1);
+//        return user;
     }
 }
